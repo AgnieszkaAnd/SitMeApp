b48f3ee9cb734997e00b9829c1165aecdad2a9df
AAndrzejczuk <agnieszkaandrzejczuk@op.pl>
2020-07-14 19:53:21 +0200

    fixes in .sql to create DB tables (2)

--- Architecture/CreateDBtables.sql
+++ Architecture/CreateDBtables.sql
@@ -150,7 +150,7 @@ IF (NOT EXISTS (SELECT *
 CREATE TABLE [Manager].[RestaurantTag] (
 [Id] uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
 [RestaurantId] uniqueidentifier NOT NULL
-	FOREIGN KEY ([RestaurantId]) REFERENCES [Client].[Reservation] ([Id]),
+	FOREIGN KEY ([RestaurantId]) REFERENCES [Manager].[Restaurant] ([Id]),
 [TagId] uniqueidentifier NOT NULL
 	FOREIGN KEY ([TagId]) REFERENCES [Manager].[Tag] ([Id]),
 )
