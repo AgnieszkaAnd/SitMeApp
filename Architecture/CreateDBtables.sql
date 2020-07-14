3bb8301543003d0d9eb81d8e2c3238b86afa579f
AAndrzejczuk <agnieszkaandrzejczuk@op.pl>
2020-07-14 19:44:41 +0200

    fixes in .sql to create DB tables

--- Architecture/CreateDBtables.sql
+++ Architecture/CreateDBtables.sql
@@ -113,8 +113,8 @@ CREATE TABLE [Client].[UserRestaurant] (
 [Id] uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
 [UserId] uniqueidentifier NOT NULL
 	FOREIGN KEY ([UserId]) REFERENCES [Client].[User] ([Id]),
-[ReservationId] uniqueidentifier NOT NULL
-	FOREIGN KEY ([ReservationId]) REFERENCES [Client].[Reservation] ([Id]),
+[RestaurantId] uniqueidentifier NOT NULL
+	FOREIGN KEY ([RestaurantId]) REFERENCES [Manager].[Restaurant] ([Id]),
 )
 
 
