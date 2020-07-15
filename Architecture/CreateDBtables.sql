ba602031dfc003341e74d01873fb90e9707de6ce
AAndrzejczuk <agnieszkaandrzejczuk@op.pl>
2020-07-15 23:57:21 +0200

    updated DB after consultation

--- Architecture/CreateDBtables.sql
+++ Architecture/CreateDBtables.sql
@@ -10,7 +10,7 @@ IF (NOT EXISTS (SELECT *
                  WHERE TABLE_SCHEMA = 'Admin' 
                  AND  TABLE_NAME = 'UserRole'))
 CREATE TABLE  [Admin].[UserRole] (
-[Id] uniqueidentifier PRIMARY KEY,
+[Id] uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
 [Name] nvarchar(20) UNIQUE,
 )
 
@@ -25,11 +25,23 @@ CREATE TABLE [Client].[User] (
 [Nick] nvarchar(100),
 [Email] nvarchar(100),
 [Password] nvarchar(100),
-[UserRoleId] uniqueidentifier NOT NULL
-	FOREIGN KEY ([UserRoleId]) REFERENCES [Admin].[UserRole] ([Id]),
 [FirstName] nvarchar(30),
 [LastName] nvarchar(50),
 [TelephoneNb] nvarchar(30)
+)
+
+
+-- USER - USER ROLE - future-proof concept allowing user to have > 1 role assigned
+IF (NOT EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'Admin' 
+                 AND  TABLE_NAME = 'UserUserRole'))
+CREATE TABLE  [Admin].[UserUserRole] (
+[Id] uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
+[UserId] uniqueidentifier NOT NULL
+	FOREIGN KEY ([UserId]) REFERENCES [Client].[User] ([Id]),
+[UserRoleId] uniqueidentifier NOT NULL
+	FOREIGN KEY ([UserRoleId]) REFERENCES [Admin].[UserRole] ([Id]),
 )
 
 
@@ -42,9 +54,8 @@ CREATE TABLE [Client].[Reservation] (
 [Id] uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
 [UserId] uniqueidentifier NOT NULL
 	FOREIGN KEY ([UserId]) REFERENCES [Client].[User] ([Id]),
-[Date] smalldatetime NOT NULL,
-[StartTime] smalldatetime NOT NULL,
-[EndTime] smalldatetime NOT NULL,
+[StartDateTime] smalldatetime NOT NULL,
+[EndDateTime] smalldatetime NOT NULL,
 [NbOfPeople] tinyint NOT NULL,
 [CustomerSpecialRequest] nvarchar(300),
 [IsConfirmed] bit NOT NULL,
@@ -72,15 +83,27 @@ CREATE TABLE [Manager].[Restaurant] (
 )
 
 
+-- TABLE TYPE
+IF (NOT EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'Manager' 
+                 AND  TABLE_NAME = 'TableType'))
+CREATE TABLE [Manager].[TableType] (
+[Id] uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
+[Name] nvarchar(30) NOT NULL
+)
+
+
 -- TABLE
 IF (NOT EXISTS (SELECT * 
                  FROM INFORMATION_SCHEMA.TABLES 
                  WHERE TABLE_SCHEMA = 'Manager' 
                  AND  TABLE_NAME = 'Table'))
-CREATE TABLE [Manager].[Table] (
+CREATE TABLE [Manager].[Table2] (
 [Id] uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
 [Name] nvarchar(50),
-[Type] nvarchar(30),
+[TableTypeId] uniqueidentifier NOT NULL
+	FOREIGN KEY ([TableTypeId]) REFERENCES [Manager].[TableType] ([Id]),
 [MaxNbOfSeats] tinyint NOT NULL,
 [Picture] image,
 [LocationInRestaurant] nvarchar(50) NOT NULL,
@@ -88,7 +111,6 @@ CREATE TABLE [Manager].[Table] (
 [RestaurantId] uniqueidentifier NOT NULL
 	FOREIGN KEY ([RestaurantId]) REFERENCES [Manager].[Restaurant] ([Id]),
 )
-
 
 -- RESERVATION-TABLE: relation many to many btw Reservations and Tables
 IF (NOT EXISTS (SELECT * 
