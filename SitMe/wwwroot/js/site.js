4d2b9def5c986b605c6724f54392b8e86f625baf
AAndrzejczuk <agnieszkaandrzejczuk@op.pl>
2020-07-26 15:05:34 +0200

    restaurant list: filters updated with JS to click + updated layout

--- SitMe/wwwroot/js/site.js
+++ SitMe/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let filterBox = document.getElementById('accordionExample');
+let filterGroup = Array.from(filterBox.children);
+
+filterGroup.forEach(function (item) {
+    console.log(item);
+    //Array.from(item.childNodes);
+
+    let subFilters = Array.from(item.querySelectorAll('a'));
+    subFilters.forEach(function(filterSelector) {
+        filterSelector.addEventListener('click', function() {
+            if (this.classList.contains('active')) {
+                this.classList.remove('active');
+            } else {
+                this.classList.add('active');
+            }
+        })
+    })
+
+});
+
+//let filters = document.getElementsByClassName('card');
+
+//let accordeonButtons = document.getElementsByClassName('list-group-item');
