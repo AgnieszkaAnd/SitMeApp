a70913a628f80e6833f77770bc3a22bf487b71bd
ola-lola <olagrzegorczyk93@gmail.com>
2020-08-28 22:16:27 +0200

    My changes

--- SitMe/Startup.cs
+++ SitMe/Startup.cs
@@ -8,7 +8,7 @@ using DataAccessLibary.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
+//using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -38,7 +38,7 @@ namespace SitMe
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
+                //app.UseBrowserLink();
             }
             else
             {
